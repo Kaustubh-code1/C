abb5145727305d86b9025ee4f465ea2abadb43ea
Kaustubh-code1 <kksitar@gmail.com>
2024-11-16 18:07:24 +0530

    Add files via upload

---
+++ Xtra_1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter your age:"))
+b=int(input("Enter your mental age:"))
+sum=a+b
+print("Sum of your physical and mental age is:", sum)
+product=a*b
+print("Product of both numbers is:", product)
+iq=int(a/b*100)
+print("Your IQ level is approximately:", iq)
+print("Thank you!")
+# IQ thing doesn't work?!?!
