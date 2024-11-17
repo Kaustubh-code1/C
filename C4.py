ebca596040d97af0a160ef521146f9f10196522e
Kaustubh-code1 <kksitar@gmail.com>
2024-11-17 12:21:00 +0530

    Add files via upload

---
+++ C4.py
@@ -0,0 +1,42 @@
+# ghp="my name is something"
+# print(ghp.capitalize())
+# print(ghp.upper())
+# print(ghp.replace("something", "Kaustubh"))
+# print(ghp.find("is"))
+# print(ghp.count("e"))
+# print(ghp.endswith("hing"))
+# print(ghp.join("&^$"))
+x=17
+y=6
+"""print("The sum of", x,"and",y,"is", x+y )
+print("The difference of",x,"and",y,"is", x-y)
+print("The product of",x,"and",y,"is", x*y)
+print("The Quotient of",x, "and",y,"is", x/y)
+print("Rounded off version:", x//y)
+print("The remainder of that is", x%y)
+print("The exponential form of",x,"and",y,"is", x**y)"""
+# All that upper chaos is arithmetic operators i think...
+"""print("Is",x,"Greater than",y,"?", x>y)
+print("Is",x,"Lesser than",y,"?", x<y)
+print("Is",x,"Greater than or equal to",y,"?", x>=y)
+print("Is",x,"Lesser than or equal to",y,"?", x<=y)
+print("Is",x,"Equal to",y,"?", x==y)
+print("Is",x,"Not equal to",y,"?", x!=y)"""
+# All this is comparison operators, i think again...
+"""x=1
+x+=12
+print(x)
+y=7
+y-=3
+print(y)
+x*=4
+print(x)
+y/=3
+print(y)
+x%=y
+print(x)"""
+# All this is Assignment operators, i think, yet again...
+"""x=36
+y=int(x**0.5)
+print(y)"""
+# THE LEGENDARY FORMULA OF SQUARE ROOT!!!
---
+++ Pr4.py
@@ -0,0 +1,13 @@
+# Hello!
+print("This is a program for finding sqaure root")
+num=int(input("Please enter the number:"))
+sqroot=num**0.5
+print("The sqaure root of your given number,",num,"is",sqroot)
+ro=sqroot//1
+print("The rounded-off version of that root is",int(ro))
+# Okay, this works.
+
+"""num2=int(input("Enter another number for cube root:"))
+curoot=num2**0.25
+print(curoot)"""
+# Ma'am this is not working!!!
