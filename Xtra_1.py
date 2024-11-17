9b3f83de9df817ade51e54242cf1b48a4aa7c4eb
Kaustubh-code1 <kksitar@gmail.com>
2024-11-17 12:33:28 +0530

    Add files via upload

--- Xtra_1.py
+++ Xtra_1.py
@@ -4,7 +4,13 @@ sum=a+b
 print("Sum of your physical and mental age is:", sum)
 product=a*b
 print("Product of both numbers is:", product)
-iq=int(a/b*100)
-print("Your IQ level is approximately:", iq)
+# iq=int(a/b*100)
+# print("Your IQ level is approximately:", iq)
 print("Thank you!")
 # IQ thing doesn't work?!?!
+
+#Retry of IQ thing on 17/11/24!
+iq_1=b/a
+iq= iq_1*100
+print("Your IQ level is approx.", iq)
+# OH MY GOD IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
