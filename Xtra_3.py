c0845d42afa126624878537f4777261d2c570856
KKaustubh <kksitar@gmail.com>
2024-11-23 20:09:02 +0530

    Add files via upload

---
+++ Xtra_3.py
@@ -0,0 +1,6 @@
+# Program to calculate circumference of a circle.
+print("This is a program to find circumference of a circle. (if π is 3.14)")
+dia=int(input("Please enter the diameter of the circle using only numbers."))
+rad=dia/2
+circ= 2*3.14*rad
+print("The circumference of a circle where diameter is",dia,"is", circ)
